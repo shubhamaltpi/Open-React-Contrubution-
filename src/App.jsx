37a62077e53135270abc18c7767e28d2a0e1c522
shubhamaltpi <shubhamaltpi@gmail.com>
2024-02-18 12:08:46 +0530

    npm start problem resolve

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
+         {/* do not change nav  */}
         <Navbar />
       </div>
     </div>
